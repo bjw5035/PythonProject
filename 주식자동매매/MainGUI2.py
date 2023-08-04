b66169e8925d677d5773e75863ea225c85bb7e5c
send2ugfd <send2ugfd@welgram.com>
2023-08-04 17:06:18 +0900

    mainGUI2 tkinter로 GUI만들기

--- 주식자동매매/MainGUI2.py
+++ 주식자동매매/MainGUI2.py
@@ -5,8 +5,20 @@ from tkinter import *
 
 
 window = tkinter.Tk()
-window.title("test")
+window.title("GUI")
 window.geometry("400x400")
+
+# 레이블을 이용한 텍스트 출력
+lb_1 = tkinter.Label(text="test")
+lb_1.pack()
+
+# 클릭시 작동하는 버튼(종료)
+def close():
+    window.destroy()
+
+button = tkinter.Button(window, overrelief="solid", width=15, repeatdelay=1000, repeatinterval=100)
+button.pack()
+
 
 window.mainloop()
 
