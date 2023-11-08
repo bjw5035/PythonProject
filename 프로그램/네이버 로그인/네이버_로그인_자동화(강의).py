eaa2863ff2be9393f6acf01aceca1dfaf3c0b262
jaewoo <send2ugfd@welgram.com>
2023-11-08 15:04:31 +0900

    코드 수정

--- 프로그램/네이버 로그인/네이버_로그인_자동화(강의).py
+++ 프로그램/네이버 로그인/네이버_로그인_자동화(강의).py
@@ -4,23 +4,24 @@ import pyautogui
 import pyperclip
 
 url = "https://nid.naver.com/nidlogin.login?mode=form&url=https%3A%2F%2Fwww.naver.com"
-browser = webdriver.Chrome('C:\chromedriver')
+# browser = webdriver.Chrome('C:\chromedriver')
+browser = webdriver.Chrome()
 browser.implicitly_wait(10)
 browser.maximize_window() # 화면 최대화
-# browser.get(url)
+browser.get(url)
 
 # 아이디 입력창
-# id = browser.find_element_by_css_selector("#id")
-# id.click()
-# pyautogui.hotkey('ctrl', 'v')
-# time.sleep(2)
+id = browser.find_element_by_css_selector("#id")
+id.click()
+pyautogui.hotkey('ctrl', 'v')
+time.sleep(2)
 
 # 비밀번호 입력창
-# pw = browser.find_element_by_css_selector("#pw")
-# pw.click()
-# pyautogui.hotkey('ctrl', 'v')
-# time.sleep(5)
+pw = browser.find_element_by_css_selector("#pw")
+pw.click()
+pyautogui.hotkey('ctrl', 'v')
+time.sleep(5)
 
 # 로그인 버튼
-# login_btn = browser.find_element_by_css_selector("#log\.login")
-# login_btn.click()
+login_btn = browser.find_element_by_css_selector("#log\.login")
+login_btn.click()
